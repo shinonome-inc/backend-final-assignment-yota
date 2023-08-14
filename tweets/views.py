599a6a2b3b143451ca2ed182cf65438536093e19
yota33166 <tamago1412-forbrst@yahoo.co.jp>
2023-08-14 14:12:15 +0900

    ログイン・ログアウト機能の実装

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, TemplateView
+
+User = get_user_model()
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "tweets/home.html"
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = User  # ユーザーモデルを指定
+    template_name = "tweets/home.html"
+    context_object_name = "user"  # テンプレート内で使用する変数名
+
+    def get_object(self):
+        return User.objects.get(username=self.request.user)
