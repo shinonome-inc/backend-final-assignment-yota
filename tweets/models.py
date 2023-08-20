155ae0cd831287a5b016162834fa764e568a3771
yota33166 <tamago1412-forbrst@yahoo.co.jp>
2023-08-20 23:07:15 +0900

    ツイートモデルの実装

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,17 @@
-# from django.db import models
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
 
 
-# class Tweet(models.Model):
+class Tweet(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField(max_length=280)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.text
