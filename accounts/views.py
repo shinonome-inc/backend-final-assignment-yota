06c7ccabc3992f76cb27b83423f2a1d7bb582c7a
yota33166 <tamago1412-forbrst@yahoo.co.jp>
2023-08-21 19:07:30 +0900

    ユーザープロフィール画面の実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,14 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, get_user_model, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from mysite.settings import LOGIN_REDIRECT_URL
+from tweets.models import Tweet
 
 from .forms import SignupForm
+
+User = get_user_model()
 
 
 class SignupView(CreateView):
@@ -19,3 +23,18 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = User  # ユーザーモデルを指定
+    template_name = "accounts/detail.html"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+    context_object_name = "profile"  # テンプレート内で使用する変数名
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.object
+        context["tweets"] = Tweet.objects.filter(author=user)
+
+        return context
--- tweets/views.py
+++ tweets/views.py
@@ -1,14 +1,11 @@
 from django.contrib import messages
-from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
-from django.views.generic import CreateView, DetailView, ListView, TemplateView
+from django.views.generic import CreateView, ListView, TemplateView
 
 from .forms import TweetForm
 from .models import Tweet
-
-User = get_user_model()
 
 
 class HomeView(LoginRequiredMixin, ListView):
@@ -19,15 +16,6 @@ class HomeView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context["tweets"] = Tweet.objects.all()
         return context
-
-
-class UserProfileView(LoginRequiredMixin, DetailView):
-    model = User  # ユーザーモデルを指定
-    template_name = "tweets/home.html"
-    context_object_name = "user"  # テンプレート内で使用する変数名
-
-    def get_object(self):
-        return User.objects.get(username=self.request.user)
 
 
 class TweetCreateView(LoginRequiredMixin, CreateView):
@@ -47,6 +35,7 @@ class TweetCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         messages.success(self.request, "記事を投稿しました。")
         return reverse("tweets:home")
+
 
 class TweetDetailView(LoginRequiredMixin, TemplateView):
     model = Tweet
