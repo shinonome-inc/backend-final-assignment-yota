912918b8a79e612e5e58dce5d2b7e55e5d5d3d68
yota33166 <tamago1412-forbrst@yahoo.co.jp>
2023-08-21 15:51:08 +0900

    Tweet作成機能の実装

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,11 @@
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView, ListView, TemplateView
+from django.urls import reverse, reverse_lazy
+from django.utils import timezone
+from django.views.generic import CreateView, DetailView, ListView
 
+from .forms import TweetForm
 from .models import Tweet
 
 User = get_user_model()
@@ -24,3 +28,22 @@ class UserProfileView(LoginRequiredMixin, DetailView):
 
     def get_object(self):
         return User.objects.get(username=self.request.user)
+
+
+class TweetCreateView(LoginRequiredMixin, CreateView):
+    model = Tweet
+    form_class = TweetForm
+    template_name = "tweets/create.html"
+    success_url = reverse_lazy("tweets:home")
+
+    def form_valid(self, form):
+        tweet = form.save(commit=False)
+        tweet.author = self.request.user
+        tweet.published_date = timezone.now()
+        tweet = form.save()
+        response = super().form_valid(form)
+        return response
+
+    def get_success_url(self):
+        messages.success(self.request, "記事を投稿しました。")
+        return reverse("tweets:home")
