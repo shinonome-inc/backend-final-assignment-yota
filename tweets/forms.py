6d28d8163c61354ff3d35ee0940a92b437136cc2
yota33166 <tamago1412-forbrst@yahoo.co.jp>
2023-08-21 17:12:47 +0900

    ツイートフォームに文字数制限のエラーメッセージを実装

--- tweets/forms.py
+++ tweets/forms.py
@@ -7,3 +7,10 @@ class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ("text",)
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if len(text) > 280:
+            raise forms.ValidationError("テキストは280文字以下で入力してください。")
+        return text
+
--- tweets/models.py
+++ tweets/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Tweet(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    text = models.TextField(max_length=280)
+    text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
