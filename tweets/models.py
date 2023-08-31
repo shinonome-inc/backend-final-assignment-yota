05b1a36ec184745280c2a968c20daa30146026e7
yota33166 <tamago1412-forbrst@yahoo.co.jp>
2023-08-31 15:33:07 +0900

    refactor: 不要メソッドの削除

--- tweets/models.py
+++ tweets/models.py
@@ -8,8 +8,5 @@ class Tweet(models.Model):
     text = models.CharField(max_length=280)
     created_at = models.DateTimeField(default=timezone.now, editable=False)
 
-    def publish(self):
-        self.save()
-
     def __str__(self):
         return self.text
